9d8e684de3ce8617c7d8a94128a966e16758ae68
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2020-08-17 10:11:57 -0400

    Initial Commit

---
+++ MyToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyToDoList
+//
+//  Created by Reagan Davenport on 8/17/20.
+//  Copyright © 2020 Reagan Davenport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
